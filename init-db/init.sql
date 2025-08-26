dddc457962f6d5c0663be1fa6aeada7d455f9020
VladSl4 <vslabidker@gmail.com>
2025-08-26 21:36:59 +0300

    alter init

--- init-db/init.sql
+++ init-db/init.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE ReceiptBot;
+GO
+
+USE ReceiptBot;
+GO
+
+CREATE LOGIN myuser WITH PASSWORD = 'mypassword';
+GO
+
+CREATE USER myuser FOR LOGIN myuser;
+GO
+
+ALTER ROLE db_owner ADD MEMBER myuser;
+GO
